50f784aa364cd016bae65ffafdbfe696b67308e7
RafaFigueiroa <rafatinsfig@gmail.com>
2024-04-03 16:30:23 -0300

    apiUrl no .env

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import express from 'express';
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient('https://mjcgotoicfvpzwfhbvuy.supabase.co', process.env.API_KEY)
+const supabase = createClient(process.env.API_URL, process.env.API_KEY)
 
 const app = express();
 app.use(cors());
