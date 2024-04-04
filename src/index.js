29f8835019b0810c0e5019b424ba9cfa02b975dc
Gabriel Lima <102686294+GabrielLima07@users.noreply.github.com>
2024-04-04 20:39:26 -0300

    modularizacao do codigo

--- src/index.js
+++ src/index.js
@@ -1,33 +1,17 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
-import { createClient } from '@supabase/supabase-js'
+import { root_router } from './api/root_router';
 
-// Create a single supabase client for interacting with your database
-const supabase = createClient(process.env.API_URL, process.env.API_KEY)
 
 const app = express();
+
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/', root_router);
 
-app.get('/users', async (req, res) => {
-    const { data, error } = await supabase.auth.signInWithPassword({
-        email: req.body.email,
-        password: req.body.password,
-      })
-
-    return res.send('usuario logado com sucesso');
-});
-
-app.post('/users', async (req, res) => {
-    const { data, error } = await supabase.auth.signUp({
-        email: req.body.email,
-        password: req.body.password,
-    })
-
-    return res.send("usuario registrado com sucesso")
-});
 
 app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`),
+  console.log(`App listening on port ${process.env.PORT}!`),
 );
